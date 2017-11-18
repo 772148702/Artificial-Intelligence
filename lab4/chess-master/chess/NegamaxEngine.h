7ee76fd75303722e52dc9ae401e3f9c5b9966a87
monster <772148702@qq.com>
2017-11-18 21:58:32 +0800

    模板

---
+++ lab4/chess-master/chess/NegamaxEngine.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "searchengine.h"
+
+class CNegamaxEngine : 	public CSearchEngine
+{
+public:
+	CNegamaxEngine(void);
+	~CNegamaxEngine(void);
+	MoveStep SearchAGoodMove(int Map[10][9]);
+protected:
+	int NegaMax(int depth);
+};
+
