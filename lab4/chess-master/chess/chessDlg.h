a91a78073a8f2228254a81fb70d36ec7907c0240
LDJ <605202637@qq.com>
2017-11-19 12:00:12 +0800

    11
    1

--- lab4/chess-master/chess/chessDlg.h
+++ lab4/chess-master/chess/chessDlg.h
@@ -23,8 +23,14 @@ public:
 	struct ChessNode{
 		CPoint pos;
 		int ChessID;
-		ChessNode(CPoint npos=CPoint(0,0),int nChessID=0){pos=npos,ChessID=nChessID;}
-		bool operator!=(const ChessNode& n){if(n.pos==this->pos && n.ChessID==this->ChessID)return false;return true;}
+		ChessNode(CPoint npos=CPoint(0,0),int nChessID=0) {
+			pos = npos, ChessID = nChessID;
+		}
+		bool operator!=(const ChessNode& n) {
+			if (n.pos == this->pos && n.ChessID == this->ChessID)
+				return false;
+			return true;
+		}
 	};
 	protected:
 	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
